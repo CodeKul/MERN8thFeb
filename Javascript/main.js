6c0f2a799fc5e2c4e3b7bec4620bd07b5240c624
chaudharisucheta <chaudharisucheta@gmail.com>
2021-04-16 13:19:02 +0530

    modules,classes

---
+++ Javascript/main.js
@@ -0,0 +1,7 @@
+function greeting() {
+  return "Good Morning";
+}
+function addNum(x, y) {
+  return x + y;
+}
+export { greeting ,addNum };  //named exports
---
+++ Javascript/person.js
@@ -0,0 +1,4 @@
+function person(firstName, city) {
+  return [firstName, city];
+}
+export default person;  //default export
