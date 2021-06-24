f10e75f92db0e6771016e52a626414424e28f520
chaudharisucheta <chaudharisucheta@gmail.com>
2021-06-24 13:07:37 +0530

    useReducer

--- Javascript/ReactJS/usereducer/src/reducerExamples/TodoList.js
+++ Javascript/ReactJS/usereducer/src/reducerExamples/TodoList.js
@@ -1,17 +1,17 @@
 import React, { useReducer, useState } from "react";
-import './Todo.css'
+
 function todoReducer(state, action) {
   switch (action.type) {
     case "add-todo":
       return {
-        todoList: [...state.todoList,
-         action.payload],
-        completed: false,
-        
+        todoList: [
+          ...state.todoList,
+          { todo: action.payload, completed: false },
+        ],
       };
     case "strike":
       return {
-        todoList:state.todoList.map((t, id) =>
+        todoList: state.todoList.map((t, id) =>
           id === action.payload ? { ...t, completed: !t.completed } : t
         ),
       };
@@ -23,7 +23,6 @@ function TodoList() {
   const [todoItem, setTodo] = useState("");
   const [state, dispatch] = useReducer(todoReducer, {
     todoList: [],
-    completed: false,
   });
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -40,15 +39,14 @@ function TodoList() {
           onChange={(e) => setTodo(e.target.value)}
         />
       </form>
+
       <div>
         {state.todoList.map((t, id) => (
-            
           <p
-            key={id}
-            onClick={() => dispatch({ type: "strike", payload: t.id })}
-            style={{textDecoration:t.completed?"line-through":""}}
+            onClick={() => dispatch({ type: "strike", payload: id })}
+            style={{ textDecoration: t.completed ? "line-through" : "" }}
           >
-            {t}
+            {t.todo}
           </p>
         ))}
       </div>
